03946856c284f3a4894b527d4033bad913110622
Mladen Turk <mturk@apache.org>
2022-09-21 15:55:20 +0200

    Initial commit

---
+++ Utils/README.md
@@ -0,0 +1,25 @@
+
+
+## Util versions:
+
+* **curl: 7.85.0**
+
+  https://curl.se/windows/dl-7.85.0_1/curl-7.85.0_1-win64-mingw.zip
+
+* **7-Zip: 22.01**
+
+  https://www.7-zip.org/a/7z2201-extra.7z
+
+* **git: 2.37.3**
+
+  https://github.com/git-for-windows/git/releases/download/v2.37.3.windows.1/PortableGit-2.37.3-64-bit.7z.exe
+
+  install:
+  ```batchfile
+  start /B /MIN /WAIT PortableGit-2.37.3-64-bit.7z.exe -o "C:\Utils\git\2.37.3" -y
+  
+  ```
+
+* **subversion: 1.14.2**
+
+  https://www.visualsvn.com/files/Apache-Subversion-1.14.2.zip
---
+++ Utils/uversions.bat
@@ -0,0 +1,23 @@
+@echo off
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem
+rem Utils component versions
+rem
+set "_GITSW_VER=2.37.3"
+set "_SVNSW_VER=1.14.2"
+set "_PERLW_VER=5.32.1.1"
+set "_NASMW_VER=2.15.05"
+set "_CMAKE_VER=3.23.3"
+set "_NINJA_VER=1.11.0"
+set "_CCLAM_VER=0.105.1"
