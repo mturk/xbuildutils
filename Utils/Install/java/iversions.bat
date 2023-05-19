c8108c9b820b4521e31e847ac61dace7cdd8396c
Mladen Turk <mturk@apache.org>
2023-05-20 01:27:19 +0200

    Upgrade versions

--- Utils/Install/java/iversions.bat
+++ Utils/Install/java/iversions.bat
@@ -14,17 +14,20 @@ rem limitations under the License.
 rem
 rem Java versions
 rem
-set "Java8Ver=362"
-set "Java8Bld=b09"
+set "Java8Ver=372"
+set "Java8Bld=b07"
 rem
-set "Java11Ver=11.0.18"
-set "Java11Bld=10"
+set "Java11Ver=11.0.19"
+set "Java11Bld=7"
 rem
-set "Java17Ver=17.0.6"
-set "Java17Bld=10"
+set "Java17Ver=17.0.7"
+set "Java17Bld=7"
 rem
 set "Java19Ver=19.0.2"
 set "Java19Bld=7"
+rem
+set "Java20Ver=20.0.1"
+set "Java20Bld=9"
 rem
 set "CurlOpts=-qkL --retry 5 --no-progress-meter"
 set "_ToolsPath=%SystemDrive%\Tools"
