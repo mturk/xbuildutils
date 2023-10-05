5a4bd379b48458f91fa152ce4c6c772e2295c4b6
Mladen Turk <mturk@apache.org>
2023-10-05 22:55:18 +0200

    Update versions

--- Utils/Install/java/iversions.bat
+++ Utils/Install/java/iversions.bat
@@ -14,19 +14,19 @@ rem limitations under the License.
 rem
 rem Java versions
 rem
-set "Java8Ver=372"
-set "Java8Bld=b07"
+set "Java8Ver=382"
+set "Java8Bld=b05"
 rem
-set "Java11Ver=11.0.20"
-set "Java11Bld=8"
+set "Java11Ver=11.0.20.1"
+set "Java11Bld=1"
 rem
-set "Java17Ver=17.0.8"
-set "Java17Bld=7"
+set "Java17Ver=17.0.8.1"
+set "Java17Bld=1"
 rem
 set "Java19Ver=19.0.2"
 set "Java19Bld=7"
 rem
-set "Java20Ver=20.0.1"
+set "Java20Ver=20.0.2"
 set "Java20Bld=9"
 rem
 set "CurlOpts=-qkL --retry 5 --no-progress-meter"
