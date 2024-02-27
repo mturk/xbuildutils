fc5a04bc05d5a4f93d495aa976961daff9893582
Mladen Turk <mturk@apache.org>
2024-02-27 19:12:11 +0100

    Upgrade cygwrun version

--- Utils/Install/iversions.bat
+++ Utils/Install/iversions.bat
@@ -17,7 +17,7 @@ rem
 set "PerlVer=5.38.2.2"
 set "PerlBld=SP_53822_64bit"
 set "NasmVer=2.16.01"
-set "CygwrunVer=1.2.1"
+set "CygwrunVer=1.2.2"
 set "CMakeVer=3.28.3"
 set "ClamAVVer=1.3.0"
 set "ClamAVBld=1.3.0"
