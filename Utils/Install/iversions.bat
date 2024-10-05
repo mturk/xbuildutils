fa72dbd7aa4112b9f99187c8259f974458c9d526
Mladen Turk <mturk@apache.org>
2024-10-05 22:18:31 +0200

    Update version

--- Utils/Install/iversions.bat
+++ Utils/Install/iversions.bat
@@ -22,7 +22,7 @@ set "CMakeVer=3.29.2"
 set "ClamAVVer=1.3.0"
 set "ClamAVBld=1.3.0"
 set "NinjaVer=1.12.0"
-set "PortableGitVer=2.45.2"
+set "PortableGitVer=2.46.2"
 rem
 rem
 set "CurlOpts=-qkL --retry 5 --no-progress-meter"
