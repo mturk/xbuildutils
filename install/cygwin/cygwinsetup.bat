174a77d41c3308e2e3c163b3791ec12f6b5df7a7
Mladen Turk <mturk@apache.org>
2024-10-06 11:36:26 +0200

    Update package list

--- install/cygwin/cygwinsetup.bat
+++ install/cygwin/cygwinsetup.bat
@@ -22,15 +22,16 @@ set "CygwinSetup=setup-x86_64-%CygwinSetupVer%.exe"
 rem
 rem Define required packages
 rem
-set "_P0=diffutils,inetutils,dos2unix,patch,patchutils,time,chere,cygrunsrv,attr,shutdown,ping,nano"
-set "_P1=cpio,unzip,xz,p7zip,zip,zstd,rpm,rpm-build"
-set "_P2=git,curl,wget,wget2,gnupg,gnupg2"
-set "_P3=python,python3,tcl,bison,byacc,flex"
-set "_P4=gcc-core,gcc-g++,autoconf,automake,autogen,autobuild,libtool,m4,make,makedepend"
-set "_P5=w32api-headers,w32api-runtime,windows-default-manifest"
-set "_P6=mingw64-x86_64-binutils,mingw64-x86_64-gcc-core,mingw64-x86_64-gcc-g++,mingw64-x86_64-headers,mingw64-x86_64-runtime"
+set "_P0=time,chere,cygrunsrv,attr,shutdown,ping"
+set "_P1=diffutils,inetutils,dos2unix,patch,patchutils,nano,gnupg2"
+set "_P2=cpio,unzip,xz,p7zip,zip,zstd,rpm,rpm-build"
+set "_P3=git,curl,wget,wget2"
+set "_P4=python,python3,tcl,bison,byacc,flex"
+set "_P5=gcc-core,gcc-g++,autoconf,automake,autogen,autobuild,libtool,m4,make,makedepend"
+set "_P6=w32api-headers,w32api-runtime,windows-default-manifest"
+set "_P7=mingw64-x86_64-binutils,mingw64-x86_64-gcc-core,mingw64-x86_64-gcc-g++,mingw64-x86_64-headers,mingw64-x86_64-runtime"
 rem
-set "CygwinPackages=%_P0%,%_P1%,%_P2%,%_P3%,%_P4%,%_P5%,%_P6%"
+set "CygwinPackages=%_P0%,%_P1%,%_P2%,%_P3%,%_P4%,%_P5%,%_P6%,%_P7%"
 rem
 set _P0=
 set _P1=
@@ -39,3 +40,4 @@ set _P3=
 set _P4=
 set _P5=
 set _P6=
+set _P7=
